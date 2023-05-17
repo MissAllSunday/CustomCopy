8ab6694bdbc69c8d5fd69f5820d4d7aa9e216b2e
Michel Mendiola <suki@missallsunday.com>
2023-05-17 12:51:50 -0600

    chore: update language strings

---
+++ languages/CustomCopy.english.php
@@ -0,0 +1,10 @@
+﻿<?php
+/**
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ */
+
+global $txt;
+
+$txt['CFL_foot_links'] = 'Put your custom copyright here:';
+$txt['CFL_foot_links_sub'] = 'Leave it empty to disable the modification. 
+You can use any HTML tag,  bbc is NOT supported.';
---
+++ languages/CustomCopy.spanish_es.php
@@ -0,0 +1,10 @@
+﻿<?php
+/**
+ * @license http://www.mozilla.org/MPL/ MPL 2.0
+ */
+
+global $txt;
+
+$txt['CFL_foot_links'] = 'Escribe tu propio copyright aquí:';
+$txt['CFL_foot_links_sub'] = 'Déjalo en blanco para desactivar la modificación. 
+Puedes usar etiquetas HTML, código BBC NO es soportado.';
